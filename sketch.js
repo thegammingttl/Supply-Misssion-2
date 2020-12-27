0e31e26d5a1f9402629ea6ddcfe769248df39d11
Vinayak Hakhu <39513628+thegammingttl@users.noreply.github.com>
2020-12-27 14:59:16 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,84 @@
+// Hi Mam i tried changing the color of the "buket thing"
+//  but it is not working :( 
+
+
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-15, width,10);
+	groundSprite.shapeColor=color("pink")
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	bottom_side = new Ground(350,670,220,20);
+	bottom_side.shapeColor = color ("red")
+	left_side= new Ground(250,620,20,120);
+	right_side= new Ground(470,620,20,120);
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.3, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+  bottom_side.display();
+  left_side.display();
+  right_side.display();
+ 
+}
+
+function keyPressed() {
+	if (keyCode === RIGHT_ARROW) {
+		helicopterSprite.x+=20;
+		Matter.Body.translate(packageBody,{x:20,y:0});
+	}
+	if (keyCode === LEFT_ARROW) {
+		helicopterSprite.x-=20;
+		Matter.Body.translate(packageBody,{x:-20,y:0});
+	}
+
+ if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody,false);
+  }
+}
+
+
+
